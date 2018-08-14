e78a17807c46ced01874b0eb031207339fd0e869
Ines Almeida <inesfralmeida@gmail.com>
2018-08-14 18:16:22 +0100

    refactor: Improve code in service worker file

--- src/sw.js
+++ src/sw.js
@@ -1,63 +1,97 @@
-var staticCacheName = 'restaurant-reviews-v3';
+const PRECACHE = 'restaurant-reviews-v1';
 
-/* When service worker installs, store key assets in cache */
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(staticCacheName).then((cache) => {
-      return cache.addAll([
-        '/',
-        'index.html',
-        'restaurant.html',
-        '/styles/styles.css',
-        '/scripts/main.js',
-        '/scripts/restaurant_info.js',
-        '/scripts/dbhelper.js',
-        '/favicon.ico',
-        '/apple-touch-icon.png',
-        'images/unavailable_small.jpg',
-        'images/unavailable_small@2x.jpg',
-        'images/unavailable_medium.jpg',
-        'images/unavailable_medium@2x.jpg',
-        'images/unavailable_large.jpg'
-      ]).then(() => self.skipWaiting()); // immediately activate new service workers
-    })
-  );
+/* ==================================== INSTALL ==================================== */
+/*              When service worker installs, store key assets in cache              */
+/* --------------------------------------------------------------------------------- */
+self.addEventListener('install', event => {
+  console.log('Installing the service worker...');
+
+  // Wait until static assets are stored to install service worker.
+  event.waitUntil(precache().then(() => {
+    // Immediately activate service worker.
+    return self.skipWaiting();
+  }));
 });
 
-/* When service worker activates, check for old cache and remove */
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all( // ensure completion of all promises
-        cacheNames.filter((cacheName) => {
-          return cacheName.startsWith('restaurant-reviews-') &&
-                 cacheName != staticCacheName;
-        }).map((cacheName) => {
-          return caches.delete(cacheName);
-        })
-      );
-    })
-  );
+/* =================================== ACTIVATE ==================================== */
+/*           When service worker activates, check for old cache and remove           */
+/* --------------------------------------------------------------------------------- */
+self.addEventListener('activate', event => {
+  console.log('Activating the service worker...');
+
+  // Wait until old cache is deleted to activate service worker.
+  event.waitUntil(deleteOldCache());
 });
 
+/* ===================================== FETCH ===================================== */
 /* Check cache for resource; if not available, fetch from network and store in cache */
-self.addEventListener('fetch', (event) => {
-  // Ignore non-GET requests: they cannot be cached using the Cache API
-  if (event.request.method !== 'GET') {
-    // console.log('[Service Worker] Fetch event ignored.', event.request.method, event.request.url);
-    return;
-  }
+/* --------------------------------------------------------------------------------- */
+self.addEventListener('fetch', event => {
+  console.log('The service worker is serving the assets...');
 
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request).then((response) => {
-        return caches.open(staticCacheName).then((cache) => {
-          cache.put(event.request, response.clone());
-          return response;
-        });
-      });
-    }).catch(() => { // log error in case of failure fetching
-      console.log('Currently experiencing difficulties accessing the network. Try again.');
-    })
-  );
+  // Ignore non-GET requests - they cannot be cached.
+  if (event.request.method !== 'GET') return;
+
+  // For all GET requests, check cache first then fetch from network and update cache.
+  event.respondWith(cacheThenNetworkAndUpdate(event.request).catch(() => {
+    console.log('Currently experiencing difficulties accessing the network. Try again.');
+  }));
 });
+
+/* =================================== FUNCTIONS =================================== */
+/* --------------------------------------------------------------------------------- */
+/**
+ * Check cache first; then fetch from network and update cache.
+ */
+const cacheThenNetworkAndUpdate = (request) => {
+  // Look for resource in cache, if it's there, serve it.
+  return caches.match(request).then(response => {
+    // If not, fetch from network.
+    return response || fetch(request).then(response => {
+      // Save new resource in cache.
+      return caches.open(PRECACHE).then(cache => {
+        cache.put(request, response.clone());
+        return response;
+      });
+    });
+  });
+};
+
+/**
+ * Check existing cache versions and delete old versions.
+ */
+const deleteOldCache = () => {
+  return caches.keys().then(cacheNames => {
+    // Ensure completion of all promises.
+    return Promise.all(
+      cacheNames.filter(cacheName => {
+        // Return any cache that is not the current cache.
+        return cacheName.startsWith('restaurant-reviews-') && cacheName !== PRECACHE;
+      }).map(cacheName => {
+        // And delete each.
+        return caches.delete(cacheName);
+      })
+    );
+  });
+};
+
+/**
+ * Store key static assets for offline-first.
+ */
+const precache = () => {
+  return caches.open(PRECACHE).then(cache => {
+    return cache.addAll([
+      '/',
+      '/index.html',
+      '/restaurant.html',
+      '/favicon.ico',
+      '/apple-touch-icon.png',
+      '/images/unavailable_large.jpg',
+      '/images/unavailable_large.webp',
+      '/scripts/main.js',
+      '/scripts/restaurant_info.js',
+      '/scripts/dbhelper.js',
+      '/styles/styles.css'
+    ]);
+  });
+};
